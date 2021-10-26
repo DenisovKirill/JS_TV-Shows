cb4db7cbd040d2250e22610176b093938cd43e1e
Kirill <kirill.denisov.khar@gmail.com>
2021-10-26 15:18:07 +0300

    'builded'

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-import './js/script.js';
+import { search } from './js/search';
+import { initPagination } from './js/pagination';
+import { logInit } from './js/login';
+import { tabsInit } from './js/tabs';
+import { modalInit } from './js/modal';
+import { favouriteInit } from './js/favourite';
+
+search();
+initPagination();
+logInit();
+tabsInit();
+modalInit();
+favouriteInit();
